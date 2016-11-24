6efd8e37fad3ce89a2fbce8e6ce0cfdeeba2a961
Gabriele M <moto.falcon.git@gmail.com>
2016-11-24 15:21:46 +0100

    Revert "falcon: Add a CDMA specific APN list"
    This commit was needed because it was impossible for CDMA users
    to edit their APNs from the settings, so having a not properly
    defined list could lead to issues. CDMA users can now edit their
    APNs if needed, so we can use the generic list.
    
    This reverts commit f351f44f92012f75dc5e7e524431dfb26814a3a2.
    
    Change-Id: I25ba4084d4cd7fc551c39f353d071055eb892d86

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,8 +49,5 @@ DEVICE_RESOLUTION := 720x1280
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
-# Release tools
-TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
-
 # inherit from the proprietary version
 -include vendor/motorola/falcon/BoardConfigVendor.mk
