765201472247f62e62b99394d83b5b62bb4eff1e
klozz <klozz707@gmail.com>
2017-09-28 19:57:00 -0400

    falcon: XPe --> xperience
    Signed-off-by: klozz <klozz707@gmail.com>

--- xpe.mk
+++ xpe.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/XPe/config/common_full_phone.mk)
+$(call inherit-product, vendor/xperience/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := xpe_falcon
